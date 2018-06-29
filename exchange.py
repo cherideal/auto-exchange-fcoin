cf2fdc8eff406237e697c5d48c2330f228572248
cherideal <cherideal@163.com>
2018-06-29 20:08:04 +0800

    add exchange file for init

---
+++ exchange.py
@@ -0,0 +1,22 @@
+from fcoin import Fcoin
+import ConfigParser
+import os
+
+cf = ConfigParser.ConfigParser()
+try:
+    cf.read("../fcoin.conf")
+    key = cf.get('api', 'key')
+    secret = cf.get('api', 'secret')
+except Exception:
+    print 'please check the config file exist'
+    exit(1)
+
+fcoin = Fcoin()
+
+print(fcoin.get_symbols())
+
+print(fcoin.get_currencies())
+
+fcoin.auth(api, secret)
+
+print(fcoin.get_balance())
